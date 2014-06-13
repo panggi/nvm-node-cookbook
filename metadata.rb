d64bc7ecc52ffa91490a5c147dcd150e1f045738
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 15:02:13 +0700

    changed to bash block

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nvm-node-cookbook"
 maintainer        "Panggi Libersa Jasri Akadol"
 maintainer_email  "panggi@me.com"
 description       "Installs nodejs using nvm 0.7.0"
-version           "0.0.4"
+version           "0.0.5"
 
 recipe "nvm-node-cookbook", "nvm node cookbook"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,12 @@
 package "curl"
 
-execute "install nodejs" do
+bash "install nodejs" do
   user "root"
-  command "curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh"
-  command "source /root/.bash_profile"
-  command "nvm install #{node.nvm.nodejs_version}"
-  command "nvm use #{node.nvm.nodejs_version}"
-  action :run
+  cwd "/root"
+  code <<-EOH
+  curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh
+  source /root/.bash_profile
+  nvm install #{node.nvm.nodejs_version}
+  nvm use #{node.nvm.nodejs_version}
+  EOH
 end
