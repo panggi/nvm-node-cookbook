a44b85971f463df5c6344f261be0724e68eb0c7c
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 14:51:25 +0700

    v0.0.4

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "nvm-node-cookbook"
 maintainer        "Panggi Libersa Jasri Akadol"
 maintainer_email  "panggi@me.com"
 description       "Installs nodejs using nvm 0.7.0"
-version           "0.0.3"
+version           "0.0.4"
 
 recipe "nvm-node-cookbook", "nvm node cookbook"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,10 @@
 package "curl"
 
-execute "install nvm" do
-  user "root"
-  command "curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh"
-  action :run
-end
-
 execute "install nodejs" do
   user "root"
+  command "curl https://raw.githubusercontent.com/creationix/nvm/v0.7.0/install.sh | sh"
   command "source /root/.bash_profile"
   command "nvm install #{node.nvm.nodejs_version}"
-  action :run
-end
-
-execute "use nodejs" do
-  user "root"
-  command "source /root/.bash_profile"
   command "nvm use #{node.nvm.nodejs_version}"
   action :run
 end
